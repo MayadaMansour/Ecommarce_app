224448054ac5f72f64eed30ac0548620d6552c66
MayadaMansour <mayadamansour1994@gmail.com>
2023-08-15 23:00:50 +0300

    Register commit

--- lib/view/modules/search/search_screen.dart
+++ lib/view/modules/search/search_screen.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget{
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body:  Center(
-        child:  Text(
-          "Search",
-          style: TextStyle(color: Colors.blue),
-        ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            )),
+      ),
+      body: const Center(
+        child: Text("Search"),
       ),
     );
   }
-
 }
